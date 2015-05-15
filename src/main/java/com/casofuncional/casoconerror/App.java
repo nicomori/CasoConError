9f236f3ecd4d9f74c371649f62b6224163214248
nicomori <nicolas.mori@gmail.com>
2015-05-15 11:27:56 -0300

    Primer commit del ejemplo basico de busqueda en google

---
+++ src/main/java/com/casofuncional/casoconerror/App.java
@@ -0,0 +1,13 @@
+package com.casofuncional.casoconerror;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
